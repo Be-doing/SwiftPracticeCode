8827b8b2763a039f5fb717872d8f574c7c277561
be-doing <1782904648@qq.com>
2021-01-03 00:15:31 +0800

    【新增】闭包语法

--- SwiftPracticeCode/BlocksOfSwift.swift
+++ SwiftPracticeCode/BlocksOfSwift.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+/**
+ 闭包是自包含的函数代码块，可以在代码中被传递和使用。Swift 中的闭包与 C 和 Objective-C 中的代码块（blocks）以及其他一些编程语言中的匿名函数（Lambdas）比较相似。
+ */
+
+// 一般形式
+/**
+ { (parameters) -> return type in
+     statements
+ }
+ */
+let defaultBlock = { (s1: String, s2: String) -> Bool in
+    return s1 > s2
+}
+
+
+/**
+ 奇淫巧技
+ */
+func mySug() {
+    let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+    // 1. 单表达式闭包的隐式返回
+    var reversedNames = names.sorted(by: { s1, s2 in s1 > s2 } )
+    // 2. 参数名称缩写
+    reversedNames = names.sorted(by: { $0 > $1 } )
+    func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+        var runningTotal = 0
+        func incrementer() -> Int {
+            runningTotal += amount
+            return runningTotal
+        }
+        return incrementer
+    }
+}
